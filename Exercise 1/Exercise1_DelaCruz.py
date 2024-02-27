7fa2c0462e560398ed2a5ae0be6f545f281a5035
Mika Patricia G. Dela Cruz <mgdelacruz16@up.edu.ph>
2024-02-27 22:02:58 +0800

    added comments and followed format

--- Exercise 1/Exercise1_DelaCruz.py
+++ Exercise 1/Exercise1_DelaCruz.py
@@ -4,30 +4,40 @@ Mika Patricia G. Dela Cruz
 2023-03941
 """
 
-print('Enter decimal here: ')
+#DECIMAL to DMS
+
+#input
+print('Enter decimal (XXX.xxx) here: ')
 decimal = input()
 
-# degrees
+#degrees (formula)
 deg = (int(float(decimal)//1))
 
-# minutes
-min = (int(((float(decimal)-float(deg))*60)//1))
-min2 = ((float(decimal)-float(deg))*60)
+#minutes (formula)
+min = (int(((float(decimal)-float(deg))*60)//1)) #this is without the decimals
+min2 = ((float(decimal)-float(deg))*60) #this is with the decimals which will be used in computing for seconds
 
-#seconds
+#seconds (formula)
 sec = (round((float(min2)-float(min))*60,2))
 
-print( deg, "-", min, "-" , sec, "-") 
+#output
+print("Here is ",decimal, "in DegMinSec: ")
+print(deg,"-",min,"-",sec) 
 
-#NEXT
+#DMS to DECIMAL
 
-print('For DMS, Enter deg here:  ')
-deg1 = input()
+#input
+print('For Enter DMS (XX-XX-XX.xx) here:  ')
+DMS = input()
 
-print('For DMS, Enter min here:  ')
-min1 = input()
+#split
+DMS1 = DMS.split("-")
 
-print('For DMS, Enter sec here:  ')
-sec1 = input()
+#assign variables
+deg1 = float(DMS1[0])
+min1 = float(DMS1[1])
+sec1 = float(DMS1[2])
 
-print(round((float(deg1) + (float(min1)/ 60) + (float(sec1)/3600)),3))
+#output (and formula)
+print("Here is", DMS, "in DECIMAL:")
+print(round(((deg1) + ((min1)/ 60) + ((sec1)/3600)),3))
