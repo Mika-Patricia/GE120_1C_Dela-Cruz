acfeeb63f31f96c3192c6a844f07a0ae214a995f
Mika Patricia G. Dela Cruz <mgdelacruz16@up.edu.ph>
2024-02-27 17:30:01 +0800

    ex 1

--- Exercise 1/Exercise1_DelaCruz.py
+++ Exercise 1/Exercise1_DelaCruz.py
@@ -4,4 +4,25 @@ Mika Patricia G. Dela Cruz
 2023-03941
 """
 
-print("HUY")
+print('Enter here: ')
+decimal = input()
+print(decimal)
+
+# degrees
+print(int(float(decimal)//1))
+
+deg = (int(float(decimal)//1))
+
+# minutes
+min = (int(((float(decimal)-float(deg))*60)//1))
+
+print(min)
+
+min2 = ((float(decimal)-float(deg))*60)
+
+#seconds
+
+sec = (round(float(min2)-float(min))*60)
+
+print(round(sec,2))
+
